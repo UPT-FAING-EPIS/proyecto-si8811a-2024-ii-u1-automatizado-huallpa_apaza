ddb106185f3a3af9a0b5576911d7b43b4835a72f
Jesus Antonio Huallpa Maron <102675596+jesushuallpaEPIS3@users.noreply.github.com>
2024-10-20 19:26:04 -0500

    archivos basura

--- tf/modules/mobile_app/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "app_bucket_name" {
-  description = "Name of the S3 bucket for the Flutter app"
-  type        = string
-}
-
-variable "environment" {
-  description = "Deployment environment"
-  type        = string
-}
--- tf/modules/mongodb/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "connection_string" {
-  value = mongodbatlas_cluster.cluster.connection_strings[0].standard
-}
-
-output "cluster_id" {
-  value = mongodbatlas_cluster.cluster.cluster_id
-}
